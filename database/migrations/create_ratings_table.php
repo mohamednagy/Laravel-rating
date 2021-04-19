af7df48c86a568b488ec29edc94271009605ff21
Mohammad Alijani <40851133+malijani@users.noreply.github.com>
2021-04-19 16:18:03 +0430

    Update create_ratings_table.php
    fixed the migration class name and add type column directly to the ratings table

--- database/migrations/create_ratings_table.php
+++ database/migrations/create_ratings_table.php
@@ -4,18 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLaravelRatingTable extends Migration
+class CreateRatingsTable extends Migration
 {
     public function up()
     {
         Schema::create('ratings', function (Blueprint $table) {
             $table->bigIncrements('id');
-
+            $table->string('type')->nullable();
             $table->morphs('model');
             $table->morphs('rateable');
             $table->decimal('value', 2, 1);
 
             $table->timestamps();
         });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
     }
 }
