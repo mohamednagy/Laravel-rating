4c9fda06ae8f0b35c1cb8e7eccaafd9353fb2883
malijani <alijanialijanvandmohammad@gmail.com>
2021-04-21 06:30:05 +0430

    Renamed ratings table migration class to CreateRatingsTable #28

--- database/migrations/create_ratings_table.php
+++ database/migrations/create_ratings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLaravelRatingTable extends Migration
+class CreateRatingsTable extends Migration
 {
     public function up()
     {
