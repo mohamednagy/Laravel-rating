ebf0592d9dff29b36be29212f45984fa36fb30a6
malijani <alijanialijanvandmohammad@gmail.com>
2021-04-21 06:52:15 +0430

    Removed add_type_column_to_ratings_table migration from tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -30,12 +30,10 @@ class TestCase extends Orchestra
 
 
         include_once __DIR__.'/../database/migrations/create_ratings_table.php';
-        include_once __DIR__.'/../database/migrations/add_type_column_to_ratings_table.php';
         include_once __DIR__.'/database/migrations/create_posts_table.php';
         include_once __DIR__.'/database/migrations/create_users_table.php';
 
-        (new \CreateLaravelRatingTable())->up();
-        (new \AddTypeColumnToRatingsTable())->up();
+        (new \CreateRatingsTable())->up();
         (new \CreatePostsTable())->up();
         (new \CreateUsersTable())->up();
     }
